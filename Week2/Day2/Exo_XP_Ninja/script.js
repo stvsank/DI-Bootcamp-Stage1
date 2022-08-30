88af5f2313eb69851655dbadcaf910daca4d35a5
Stvsank <stvsank@gmail.com>
2022-08-30 20:45:47 +0000

    W2_D2_Exo_Ninja

--- Week2/Day2/Exo_XP_Ninja/script.js
+++ Week2/Day2/Exo_XP_Ninja/script.js
@@ -3,10 +3,10 @@
 	//trouvez la date à laquelle la plus jeune a exactement 
 	//la moitié de l'âge de la plus âgée. 
 	//Notes : Les dates sont données au format AAAA 
-	Date1 = Number(prompt("Entrez l'année de naissance de la première personne", "1999"));
-	Date2 = Number(prompt("Entrez l'année de naissance de la deuxième personne", "2009"));
-	age1 = 2022 - Date1;
-	age2 = 2022 - Date2;
+	date1 = Number(prompt("Entrez l'année de naissance de la première personne", "1999"));
+	date2 = Number(prompt("Entrez l'année de naissance de la deuxième personne", "2009"));
+	/*age1 = 2022 - date1;
+	age2 = 2022 - date2;
 	//soit x etant l'age de la personne la plus jeune et soit y etant l'âge de la personne la plus vielle
 	if (age1 != age2 ) {
 		if (age1 < age2) {
@@ -34,11 +34,21 @@
 		}
 	} else {
 		console.log("Vous avez entrez deux dates identiques");
+	}*/
+	//autre résolution
+	if (date1>date2) {
+		let x = 2*date1 - date2;
+		console.log(x);
+	} else if (date1<date2) {
+		let x = 2*date2 - date1;
+		console.log(x);
+	} else {
+		console.log("Les deux dates sont identiques");
 	}
-
+	
 //Exercice 2 : Codes Postaux
-	//1ère partie
 	let code = prompt("Quel est votre code postal?","xxxxx");
+	//1ère partie
 	let codeNumb =Number(code);
 	if (code.length == 5 &&  codeNumb) {
 		console.log("Succès");
@@ -46,8 +56,22 @@
 		console.log("Error");
 	}
 	//2ieme partie
+	if (/[0-9][0-9][0-9][0-9][0-9]$/y.test(code)) {
+		console.log("Succès");
+	} else {
+		console.log("Error");
+	}
 
-
-
-
-
+//Exercice 3 : Mot Secret
+	//1
+	let mot = prompt("Entrez un mot","yooo");
+	//2
+	mot = mot.replace(/[a,e,i,o,u]+/g,"");
+	console.log(mot);
+	//3
+	mot = mot.replace(/[a]+/g,"1");
+	mot = mot.replace(/[e]+/g,"2");
+	mot = mot.replace(/[i]+/g,"3");
+	mot = mot.replace(/[o]+/g,"4");
+	mot = mot.replace(/[u]+/g,"5");
+	console.log(mot);
