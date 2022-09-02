63a72817c9d0fb6f0926429d213df4bb4bd3d660
Stvsank <stvsank@gmail.com>
2022-09-02 10:13:03 +0000

    W2_D3_Defi

--- Week2/Day3/Defi/index.js
+++ Week2/Day3/Defi/index.js
@@ -1,1 +1,14 @@
-//
+//Une boucle
+	let affiche = "* ";
+	for(let i = 0; i < 6; i++) {
+		console.log(affiche);
+		affiche += "* ";
+	}
+//Deux boucle
+	let affiche = "* ";
+	for(let i = 0; i < 4; i++) {
+		for(let j = 0; j < i; j++) {
+			console.log(affiche);
+			affiche += "* ";
+		}
+	}
