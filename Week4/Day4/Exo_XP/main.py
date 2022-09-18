a0cb74931f798ae365bc1dcf031f8f76c62d8bf0
stvsank@gmail.com <stvsank@gmail.com>
2022-09-18 12:21:13 +0000

    W4_D4_Exo_XP

---
+++ Week4/Day4/Exo_XP/main.py
@@ -0,0 +1,86 @@
+# Exercice 1 : Qu'apprenez-Vous ?
+def display_message() :
+    print("J'apprends à utilisez les fonctions Python")
+display_message()
+
+# Exercice 2 : Quel Est Votre Livre Préféré ?
+def favorite_book(title) :
+    print(f"One of my favorite books is {title}")
+favorite_book("Candide")
+
+#Exercice 3 : Un Peu De Géographie
+def describe_city(city,country = "Burkina Faso") :
+    print(f"{city} is in {country}")
+describe_city("Ouagadougou")
+
+# Exercice 4 : Aléatoire
+import random
+def aleatoire(number) :
+    alea = random.randint(1,100)
+    if alea == int(number) :
+        print("Success")
+    else :
+        print("Try again")
+    print("Votre nombre :",number)
+    print("Nombre aléatoire :",alea)
+aleatoire(52)
+
+# Exercice 5 : Créons Des Chemises Personnalisées !
+#1...4
+def make_shirt(size = "XXL",text = "I love Python") :
+    print(f"The size of the shirt is {size} and the text is {text}")
+#5
+make_shirt()
+#6
+make_shirt("M")
+#7
+make_shirt("L","SSS")
+#8
+make_shirt(size = "S",text = "SSS")
+
+# Exercice 6 : Magiciens
+magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+def show_magicians(magician_names) :
+    list(filter(lambda a: print(a),magician_names))
+def make_great(liste) :
+    for i in range(0,len(liste)):
+        liste[i] += " the Great"
+make_great(magician_names)
+show_magicians(magician_names)
+
+#Exercice 7 : Conseils De Température
+
+def get_random_temp(saison) :
+    import random
+    if saison.lower() == "hiver" :
+        return random.randint(-10, 16)
+    elif saison.lower() == "autonne" :
+        return random.randint(10, 25)
+    elif saison.lower() == "printemps" :
+        return random.randint(0, 15)
+    elif saison.lower() == "été" :
+        return random.randint(20, 40)
+    else :
+        print('Error, default = 0')
+        return 0
+
+def main() :
+    temp = get_random_temp(input("Saisissez une saison : "))
+    print(f"The temperature right now is {temp} degrees Celsius")
+    if temp < 0 :
+        print("Brrr, that’s freezing! Wear some extra layers today")
+    elif temp < 16 :
+        print("Quite chilly! Don’t forget your coat")
+    elif temp <= 23 :
+        print("The temperature is ideal")
+    elif temp < 32 :
+        print("You can go to the beach")
+    elif temp < 40 :
+        print("It's very hot")
+    else:
+        print("Dangerous")
+
+main()
+
+
+
