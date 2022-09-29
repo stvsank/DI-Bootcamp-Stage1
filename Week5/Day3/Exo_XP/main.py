804452d42eb9b87e1b819e35cba31e1f6c3f9c48
stvsank@gmail.com <stvsank@gmail.com>
2022-09-29 23:04:47 +0000

    W5_D3_Exo_Xp

--- Week5/Day3/Exo_XP/main.py
+++ Week5/Day3/Exo_XP/main.py
@@ -19,8 +19,37 @@ class Currency:
         self.currency = currency
         self.amount = amount
 
+    def __str__(self):
+        if self.amount > 1 :
+            return f"{self.amount} {self.currency}s"
+        else :
+            return f"{self.amount} {self.currency}"
+
+    def __repr__(self):
+        if self.amount > 1 :
+            return f"{self.amount} {self.currency}s"
+        else :
+            return f"{self.amount} {self.currency}"
+
+    def __int__(self):
+        return self.amount
+
     def __add__(self, other):
-        return self.amount + other.amount
+        a=True
+        try :
+            if self.currency == other.currency :
+                print(self.amount + other.amount)
+                return Currency(self.currency,self.amount + other.amount)
+            else :
+                a=False
+        except :
+            if a :
+                print(self.amount + other)
+                return Currency(self.currency,self.amount + other)
+        if not a :
+            raise TypeError("Cannot add between Currency type <dollar> and <shekel>")
+
+
 
 
 c1 = Currency('dollar', 5)
@@ -28,4 +57,23 @@ c2 = Currency('dollar', 10)
 c3 = Currency('shekel', 1)
 c4 = Currency('shekel', 10)
 
-print(c1 + c2)
+print(str(c1))
+
+print(int(c1))
+
+print(repr(c1))
+
+c1 + 5
+
+c1 + c2
+
+print(c1)
+
+c1 += 5
+
+print(c1)
+
+c1 += c2
+
+print(c1)
+
