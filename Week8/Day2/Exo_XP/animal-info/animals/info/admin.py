3e1c13c9973a48c66008f10c71a63c454ba06bf5
Steve Sankara <stvsank@gmail.com>
2022-10-22 01:07:13 +0000

    relation one to many

--- Week8/Day2/Exo_XP/animal-info/animals/info/admin.py
+++ Week8/Day2/Exo_XP/animal-info/animals/info/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Family, Animal #import the Person model
 
 # Register your models here.
+admin.site.register(Family)
+admin.site.register(Animal)
--- Week8/Day2/Exo_XP/animal-info/animals/info/urls.py
+++ Week8/Day2/Exo_XP/animal-info/animals/info/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('family/<int:X>', views.randomq, name='randomq'),
-  path('animal/<int:X>/', views.question, name='question'),
-  path('animals/', views.questions, name='questions')
+  path('family/<int:x>', views.family, name='family'),
+  path('animal/<int:x>/', views.animal, name='animal'),
+  path('animals/', views.animals, name='animals')
 ]
