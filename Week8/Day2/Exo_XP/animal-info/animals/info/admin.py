66c48e1df6eda76e69fedaf07bbbb49b01ed56f9
Steve Sankara <stvsank@gmail.com>
2022-11-18 01:18:55 +0000

    div

--- Week8/Day2/Exo_XP/animal-info/animals/info/admin.py
+++ Week8/Day2/Exo_XP/animal-info/animals/info/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Family, Animal #import the Person model
+from .models import Family, Employe #import the Person model
 
 # Register your models here.
 admin.site.register(Family)
-admin.site.register(Animal)
+admin.site.register(Employe)
