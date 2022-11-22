2ca7d7730a9f9b9a44ca8a1da6eca295f20c74b1
Steve Sankara <stvsank@gmail.com>
2022-11-22 08:23:45 +0000

    gifs

--- Week8/Day3/Exo_XP/gifs/models.py
+++ Week8/Day3/Exo_XP/gifs/models.py
@@ -5,12 +5,18 @@ from django.db import models
 class Gif(models.Model):
     title = models.CharField(max_length=30)
     url = models.URLField()
-    uploader_name = models.FloatField()
-    created_at = models.FloatField()
+    uploader_name = models.CharField(max_length=30)
     created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}'
 
 class Category(models.Model):
 	name = models.CharField(max_length=30)
 	gif = models.ManyToManyField(Gif)
+
+	def __str__(self):
+		return f'{self.name}'
+
 
 
